4ce8c25dfa9dd28788dae44476ff905d7984b7ee
Egor Menshikov <r3born.fs@gmail.com>
2023-07-06 21:32:05 +0300

    sem 3 done

---
+++ src/Sem_3/Task_1/Main.java
@@ -0,0 +1,13 @@
+package Sem_3.Task_1;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(5);
+        list.add(3);
+        list.add(7);
+        list.print();
+
+    }
+}
---
+++ src/Sem_3/Task_1/QSort.java
@@ -0,0 +1,50 @@
+package Sem_3.Task_1;
+
+public class QSort {
+    public static void main(String[] args) {
+        int[] array = new int[]{4, 2, 5, 8, 1, 9, 2, 3, 6, 8, 5};
+
+        quickSort(array);
+        printArray(array);
+    }
+
+    public static void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    public static void quickSort(int[] array, int startPosition, int endPosition) {
+        int leftPosition = startPosition;
+        int rightPosition = endPosition;
+        int pivot = array[(startPosition + endPosition) / 2];
+        do {
+            while (array[leftPosition] < pivot) {
+                leftPosition++;
+            }
+            while (array[rightPosition] > pivot) {
+                rightPosition--;
+            }
+            if (leftPosition <= rightPosition) {
+                if (leftPosition < rightPosition) {
+                    int temp = array[leftPosition];
+                    array[leftPosition] = array[rightPosition];
+                    array[rightPosition] = temp;
+                }
+                leftPosition++;
+                rightPosition--;
+            }
+        } while (leftPosition <= rightPosition);
+        if (leftPosition < endPosition) {
+            quickSort(array, leftPosition, endPosition);
+        }
+        if (startPosition < rightPosition) {
+            quickSort(array, startPosition, rightPosition);
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
+
